c02c73a08ab476d9a9c44d459cff138378666658
KiranGopalakrishnan <kirangplkrishnan@gmail.com>
2017-05-21 11:31:51 -0400

    completed

--- app/src/main/java/com/example/nijin/airbnb/MainActivity.java
+++ app/src/main/java/com/example/nijin/airbnb/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.nijin.airbnb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.getSupportActionBar().hide();
+        TextView tv1 = (TextView) findViewById(R.id.btn);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,homes.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nijin/airbnb/homes.java
+++ app/src/main/java/com/example/nijin/airbnb/homes.java
@@ -1,7 +1,10 @@
 package com.example.nijin.airbnb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class homes extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class homes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homes);
+        this.getSupportActionBar().hide();
+        TextView tv1 = (TextView) findViewById(R.id.btn);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(homes.this,places.class);
+                startActivity(i);
+            }
+        });
     }
 }
